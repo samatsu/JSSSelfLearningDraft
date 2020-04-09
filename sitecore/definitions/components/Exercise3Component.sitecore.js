634fd4868622c07546c8dd43650ea90df1f42c27
samatsu <samatsu@outlook.jp>
2020-04-09 05:27:35 -0700

    Exercise 3

---
+++ sitecore/definitions/components/Exercise3Component.sitecore.js
@@ -0,0 +1,7 @@
+import { Manifest, ComponentDefinition } from '@sitecore-jss/sitecore-jss-manifest';
+
+export default function(manifest){
+    manifest.addComponent({
+        name: 'Exercise3Component'
+    });
+}
---
+++ src/components/Exercise3Component/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Exercise3Component = (props) => {
+    return <React.Fragment>
+        <div>
+            <p>Exercise3 Static Component</p>
+            <div>Hello World! - Static Component</div>
+        </div>
+    </React.Fragment>
+}
+
+export default Exercise3Component;
