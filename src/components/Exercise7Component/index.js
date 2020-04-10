ba6625115d1353f1b5c4b8346feb36c7daf5262f
samatsu <samatsu@outlook.jp>
2020-04-09 19:58:47 -0700

    fix Exercise 7 issue

--- src/components/Exercise7Component/index.js
+++ src/components/Exercise7Component/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Text, Image , withSitecoreContext } from '@sitecore-jss/sitecore-jss-react';
+import { Text, Image , withSitecoreContext, RichText } from '@sitecore-jss/sitecore-jss-react';
 
 const Exercise7Component = (props) => (
   <div>
     <p>Route Level Field</p>
     <Text field={props.sitecoreContext.route.fields.newsTitle} />
-    <Text field={props.sitecoreContext.route.fields.newsDescription} />
+    <RichText field={props.sitecoreContext.route.fields.newsDescription} />
     <Image field={props.sitecoreContext.route.fields.newsImage} />
   </div>
 );
